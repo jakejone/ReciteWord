8662a120ccae401a5f1e6c2345343708ea5cf401
jake <jake.xyj@gmail.com>
2024-03-26 11:05:13 +0800

    Initial Commit

---
+++ ReciteWords/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReciteWords
+//
+//  Created by jake on 2024/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReciteWords/ReciteWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciteWordsApp.swift
+//  ReciteWords
+//
+//  Created by jake on 2024/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ReciteWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
